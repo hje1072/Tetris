e333c8a6a237a14be6a5b89f82e816d00965d2e9
Babydinos <70854950+jyj7652@users.noreply.github.com>
2024-04-02 16:38:05 +0900

    쾅 구현
    일시정지키 p 로 바꿨음. 
    스페이스바를 눌렀을 때, 바로내려옴

---
+++ Tetris/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		//창띄우기
+		JFrame window = new JFrame("Tetris");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		
+		//게임창 크기 설정용
+		GamePanel gp = new GamePanel();
+		window.add(gp);
+		window.pack();
+		
+		//중앙에 띄워서 보여줘라.
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		
+		
+		
+		//스레드 돌려서 게임돌리기
+		gp.launchGame();
+		
+		
+	}
+}
